863f756be7bdd2824d15687f93841620d7a81330
Neil <thanhdao.pv1999@gmail.com>
2024-10-06 14:11:54 +0700

    Updat eslint

--- fe/eslint.config.mjs
+++ fe/eslint.config.mjs
@@ -3,8 +3,27 @@ import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 12,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: 'prettier',
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      eqeqeq: 'error',
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]
